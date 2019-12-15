4b9eb4966cf08b789d83e1c25ff71585e478cb3c
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-15 17:58:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.set('view engine','ejs');
 
-	{title: 'undefined', description: 'undefined', picture :'undefined'}
-);
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
