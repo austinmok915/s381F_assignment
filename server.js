c76a595d724d9cca17b2e0073c64513725456403
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2019-12-15 17:15:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/upload', (req,res) => {
 
 app.post('/upload', (req,res) => {
 	photo.title = req.body.title;
-	photo.description = req.body.description;)
-	photo.picture = new Buffer(fs.readFileSync(req.body.filetoupload.path)).toString("base64")
+	photo.description = req.body.description;
+	photo.picture = new Buffer(fs.readFileSync(req.body.filetoupload.path)).toString("base64");
 	console.log(photo.picture);
 	res.redirect('/display');
 });
