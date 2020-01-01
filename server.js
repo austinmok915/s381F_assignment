7495e2668c8a7e950706d3ed00aba30f811bc1fb
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2020-01-01 10:57:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,30 +3,53 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const multer = require("multer");
 
-app.set('view engine','ejs');
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  location: "/pic/"
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-	res.redirect('/upload');
+app.get("/", express.static(path.join(__dirname, "./views")));
+
+app.post('/upload', upload.single("image"), (req,res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./pic/image.png");
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    }
+  }
+);
 });
 
-app.get('/upload', (req,res) => {
-	res.status(200).render('upload');
-});
-
-app.post('/upload', (req,res) => {
-	res.redirect('/display');
-});
-
-app.get('/display', (req,res) => {
-	res.status(200).render('display',{c: req.body.title, n:req.body.description});
-});
-
-app.post('/display', (req,res) => {
-	res.status(200).render('display');
+app.get('/image.png', (req,res) => {
+	res.sendFile(path.join(__dirname, "./uploads/image.png"));
 });
 
 app.listen(process.env.PORT || 8099);
