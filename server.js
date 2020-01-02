829fda299697b28c2fecf352699456342b066acc
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2020-01-02 13:20:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','ejs');
 
-var title = null;
-var description = null;
-var mimetype = "images/jpeg";
-
 app.get('/', (req,res) => {
 	res.redirect('/filetoupload');
 });
@@ -20,14 +16,23 @@ app.get('/filetoupload', (req,res) => {
 	res.status(200).render('filetoupload');
 });
 
-app.post('/filetoupload', upload.array('filetoupload'), (req,res) => {
-        let formData = req.body;
-	console.log(formData);
-	res.redirect('/display');
-});
-
-app.get('/display', (req,res) => {
-	res.status(200).render('display', {t: title, d: description});
+app.post('/filetoupload' , (req,res) => {
+	const form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) => {
+      		const filename = files.filetoupload.path;
+		console.log(filename);
+      		if (fields.title && fields.title.length > 0) {
+        		title = fields.title;
+      		}
+		if (fields.description && fields.description.length > 0) {
+        		description = fields.description;
+      		}
+		console.log("1");
+		fs.readFile(files.filetoupload.path, (err,data) => {
+          		image = new Buffer.from(data).toString('base64');
+		});
+	});
+	res.status(200).render('filetoupload', {t :title, d :description});
 });
 
 app.listen(process.env.PORT || 8099);
