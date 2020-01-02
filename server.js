77cba1c7cdbc35d9e933a9bfedb27267adf2a1e2
austinmok915 <36772903+austinmok915@users.noreply.github.com>
2020-01-02 13:24:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-
 const fs = require('fs');
 const formidable = require('formidable');
 const express = require('express');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine','ejs');
 
 app.get('/', (req,res) => {
